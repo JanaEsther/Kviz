0a309f2f23b0e71ee366f6e4473a41cacbad4fa1
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-08-04 11:50:36 +0200

    asnwer_oprava

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const answer = prompt("Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.");
+const questionElement = document.querySelector('.question');
+
+const question =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
+
+questionElement.textContent = question;
+const userAnswer = prompt(question);
 
 const answerElement = document.querySelector('.answer__text');
-const puvodniObsah = answerElement.textContent;
-answerElement.textContent = answer;
+answerElement.textContent = userAnswer;
 
-const answerCheck = document.querySelector('.answer');
-if(answer === "Bell"){
-  answerCheck.classList.add('answer--correct');
-}else{
-  answerCheck.classList.add('answer--wrong');
+const AnswerElement = document.querySelector('.answer');
+
+if (userAnswer === 'Bell') {
+  AnswerElement.classList.add('answer--correct');
+} else {
+  AnswerElement.classList.add('answer--wrong');
 }
