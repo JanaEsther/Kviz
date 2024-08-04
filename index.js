90e2261354d51925e73015c2914c97e30f41254b
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-08-04 10:24:59 +0200

    js_answer

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const answer = prompt("Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.");
 
+const answerElement = document.querySelector('.answer__text');
+const puvodniObsah = answerElement.textContent;
+answerElement.textContent = answer;
+
+const answerCheck = document.querySelector('.answer');
+if(answer === "Bell"){
+  answerCheck.classList.add('answer--correct');
+}else{
+  answerCheck.classList.add('answer--wrong');
+}
